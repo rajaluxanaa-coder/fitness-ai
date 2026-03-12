0fb982fa6a497236397c842ff8268f7d19af5c6b
Rajaluxanaa <rajaluxanaa@gmail.com>
2026-03-13 01:39:27 +0530

    Fixed WorkoutLog field name mismatch

--- app.py
+++ app.py
@@ -625,7 +625,7 @@ def log_workout():
     workout = WorkoutLog(
         user_id=session['user_id'],
         date=datetime.now().date(),
-        workout_type=data['workout_type'],
+        workout_name=data['workout_type'],
         duration=data['duration'],
         calories_burned=data['calories_burned'],
         exercises=data.get('excercise',''),
